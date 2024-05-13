6942cdac4110ec4e340ada737af905ccd3c9c3ec
Alex <alex.kleinman@gmail.com>
2024-05-13 13:10:53 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(distance){
+
+let cost; 
+if (distance <= 400) {
+  cost = 'This one is on me!'; 
+}
+else if (distance > 400 && distance <= 2000) {
+  cost = 'That will be twenty bucks.';
+}
+else if (distance > 2000 && distance < 2500) {
+  cost = 'I will gladly take your thirty bucks.';
+}
+else if (distance > 2500) { 
+  cost = 'No can do.';
+}
+return cost;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+
+  let charmLevel; 
+
+  switch (tip) {
+    case 'generous':
+      charmLevel = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      charmLevel = 'Thank you.';
+      break;
+    default: 
+      charmLevel = 'Bye.';
+      break;
+  }
+return charmLevel;
 }
